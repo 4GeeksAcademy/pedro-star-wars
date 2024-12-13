3a6b899bd3205c4fe235d6345bc78bcffcf186fd
p3drac0 <75147146+p3drac0@users.noreply.github.com>
2024-12-13 19:07:25 +0000

    install react router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import App from "./app";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -9,7 +11,9 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
 
